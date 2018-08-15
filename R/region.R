5e0ef61670b5f74f01743011ccb8afd9a5d9645c
Yulong Niu <yulong.niu@hotmail.com>
2018-08-16 00:37:54 +0800

    update OverlapRegion functions

--- R/region.R
+++ R/region.R
@@ -3,49 +3,55 @@
 ##' \itemize{
 ##'   \item \code{OverlapRegionRate()}: Check if extended regions have intersections with overlap rates.
 ##'   \item \code{OverlapRegion()}: Check regions have interactions
-##'   \item \code{SortMat()}: Sort the 1st and 2nd columns resulting in 1st column is less than or equal to that of 2nd column.
+##'   \item \code{SortRegion()}: Sort the 1st and 2nd columns resulting in 1st column is less than or equal to that of 2nd column.
 ##' }
 ##' @title Overlapped regions
 ##' @param regionf A \code{numeric} vector with length two, and the value of 1st position is smaller/equal to that of 2nd position.
-##' @param regionMat A \code{tbl_df} with 2 columns. In the \code{OverlapRegionRate()} function, the rows should have overlap with the input \code{reginf}. The 1st column should be equal or smaller than the 2nd column, otherwise use the \code{SortMat()} sort the \code{regionMat} at first.
+##' @param regionMat A \code{tbl_df} with 2 columns. In the \code{OverlapRegionRate()} function. The 1st column should be equal or smaller than the 2nd column, otherwise use the \code{SortRegion()} sort the \code{regionMat} at first.
 ##' @return
 ##' \itemize{
 ##'   \item \code{OverlapRegionRate()}: A \code{logic} value.
-##'   \item \code{OverlapRegion()}: A \code{numeric} matrix with two columns. 1st column is the overlap rate of `regionf`, 2nd column is the overlap rate of `regionMat`.
-##'   \item \code{SortMat()}: A same object as the input \code{regionMat}
+##'   \item \code{OverlapRegion()}: A \code{numeric} matrix with four columns. 1st column is the overlap rate of `regionf`, 2nd column is the overlap rate of `regionMat`, 3rd and 4th columns are intersection start and end regions.
+##'   \item \code{SortRegion()}: A same object as the input \code{regionMat}
 ##' }
 ##' @examples
 ##' require('magrittr')
 ##'
-##' tMat <- tibble(from = c(1, 103, 111, 8, 10), to = c(101, 112, 1000, 49, 86))
+##' tMat <- tibble(start = c(1, 103, 111, 49, 10), end = c(101, 112, 1000, 8, 86)) %>% SortRegion
 ##' tReg <- c(100, 110)
 ##'
 ##' ## overlapped regions
-##' inLogic <- OverlapRegion(tReg, tMat, extend = 0)
+##' OverlapRegion(tReg, tMat, extend = 0)
 ##'
 ##' ## overlap rate
-##' OverlapRegionRate(tReg, tMat[inLogic, ])
+##' OverlapRegionRate(tReg, tMat)
+##'
+##' ## sort matrix at first
+##'
 ##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
+##' @importFrom magrittr %>% %<>%
+##' @importFrom dplyr mutate if_else select
 ##' @rdname overlapregion
 ##' @export
 ##'
 OverlapRegionRate <- function(regionf, regionMat) {
 
-  ## overlap rate, 1st extract overlapped regions
-  tLen <- regionMat[, 2] - regionMat[, 1]
-  maxfLogic <- regionMat[, 1] < regionf[1]
-  regionMat[maxfLogic, 1] <- regionf[1]
-  mintLogic <- regionMat[, 2] > regionf[2]
-  regionMat[mintLogic, 2] <- regionf[2]
+  ## step1: calculate inter length
+  regionMat %<>%
+    mutate(fLen = max(regionf) - min(regionf)) %>%
+    mutate(tLen = end - start) %>%
+    mutate(maxstart = if_else(start > min(regionf), start, min(regionf))) %>%
+    mutate(minend = if_else(end < max(regionf), end, max(regionf))) %>%
+    mutate(interLen = minend - maxstart) %>%
+    mutate(interLen = if_else(interLen < 0, 0, interLen))
 
-  ## 2nd inter region
-  interLen <- regionMat[, 2] - regionMat[, 1]
-  fLen <- regionf[2] - regionf[1]
+  ## step2: calculate rate
+  regionMat %<>%
+    mutate(fRate = interLen / fLen) %>%
+    mutate(tRate = interLen / tLen) %>%
+    select(fRate, tRate, maxstart:minend)
 
-  overlapRate <- cbind(fRate = interLen/fLen,
-                       tRate = interLen/tLen)
-
-  return(overlapRate)
+  return(regionMat)
 }
 
 
@@ -59,29 +65,36 @@ OverlapRegionRate <- function(regionf, regionMat) {
 OverlapRegion <- function(regionf, regionMat, extend = 100) {
 
   regionMat %<>%
-    mutate(from = if_else(from - extend > 0, from - extend, 0)) %>%
-    mutate(to = to + extend)
+    mutate(start = if_else(start > extend, start - extend, 0)) %>%
+    mutate(end = end + extend)
 
   olLogic <- regionMat %>%
-    transmute(from > max(regionf) | to < min(regionf)) %>%
+    transmute(start > max(regionf) | end < min(regionf)) %>%
     unlist %>%
     unname
 
   return(!olLogic)
 }
 
+
+
 ##' @inheritParams OverlapRegionRate
 ##' @rdname overlapregion
+##' @importFrom magrittr %<>% %>%
+##' @importFrom mutate transmute if_else bind_cols
 ##' @export
 ##'
-SortMat <- function(regionMat) {
+SortRegion <- function(regionMat) {
 
-  ## sort regionMat
-  sortLogic <- regionMat[, 1] > regionMat[, 2]
-  if (sum(sortLogic) > 0) {
-    regionMat[sortLogic, 1:2, drop = FALSE] <- regionMat[sortLogic, 2:1, drop = FALSE]} else {}
+  start <- regionMat %>%
+    transmute(start = if_else(start < end, start, end))
 
-  return(regionMat)
+  end <- regionMat %>%
+    transmute(end = if_else(end > start, end, start))
+
+  reg <- bind_cols(start, end)
+
+  return(reg)
 }
 
 
