4d9a4f413125ca8ab1a0f92cec29457a746ce8c4
Yulong Niu <yulong.niu@hotmail.com>
2018-08-28 00:20:03 +0800

    update merge functions

--- R/anno_template.R
+++ R/anno_template.R
@@ -59,31 +59,24 @@
 ##   return(cnvTable)
 ## }
 
+
+
 ##' The CNV summary genes table.
 ##'
 ##' ## column names for OMIM
 ##' \itemize{
 ##'   \item CNV
-##'   \item OverlapRelation
-##'   \item Gene.Symbols
+##'   \item overlap_relation
+##'   \item gene_symbol
 ##'   \item Phenotypes
-##'   \item Phenotypes.CN
-##'   \item genetic???
-##'   \item Mim.Number
+##'   \item Phenotypes CN
+##'   \item MIMNumber
+##'   \item MIMType
 ##' }
 ##' ## column names for ClinGenTriHaplo
 ##' \itemize{
-##'   \item Haploinsufficiency.Score
-##'   \item Haploinsufficiency.PMID1
-##'   \item Haploinsufficiency.PMID2
-##'   \item Haploinsufficiency.PMID3
-##'   \item Triplosensitivity.Score
-##'   \item Triplosensitivity.PMID1
-##'   \item Triplosensitivity.PMID2
-##'   \item Triplosensitivity.PMID3
-##'   \item Date.Last.Evaluated
-##'   \item Loss.phenotype.OMIM.ID
-##'   \item Triplosensitive.phenotype.OMIM.ID
+##'   \item Haploinsufficiency Score
+##'   \item Triplosensitivity Score
 ##' }
 ##'
 ##' ## column names for DECIPHERHaplo
@@ -98,16 +91,15 @@
 ##'
 ## column names for DDG2P
 ##' \itemize{
+##'   \item gene.mim
 ##'   \item disease.name
+##'   \item disease.mim
 ##'   \item DDD.category
 ##'   \item allelic.requirement
 ##'   \item mutation.consequence
 ##'   \item phenotypes
-##'   \item organ.specificity.list
 ##' }
 ##'
-##' ## example column (has blanks)
-##' ## chr3:162819884-162903642	CNVinGene	HDLCQ5	[High density lipoprotein cholesterol level QTL 5], 610761 (2)			610761	40				40				2014-03-12			0.020830666	0.003227266288348
 ##' @title Merge genes database
 ##' @param gdbList A list containing CNV genes database. 1st element is the OMIM database, 2nd is the ClinGenTriHaplo, 3rd is the DECIPHERHaplo, 4th is the ExACpLI, 5th is the DECPHER_DDG2P
 ##' @return A \code{data.frame}
@@ -116,166 +108,101 @@
 ##' data(CNVdb)
 ##' data(kit)
 ##'
-##' refGene <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$RefGeneGRCh37)
+##' refGene <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$RefGeneGRCh37, n = 2)
 ##' gdbList <- list()
-##' gdbList[[1]] <- AnnoCNVGeneRefGene2OMIM(refGene[[1]], CNVdb$OMIMGRCh37)[[1]]
-##' gdbList[[2]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ClinGen_TriHaploGRCh37)[[1]]
-##' gdbList[[3]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$DECIPHER_Haplo)[[1]]
-##' gdbList[[4]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ExAC_pLI)[[1]]
+##' gdbList[[1]] <- AnnoCNVGeneRefGene2OMIM(refGene[[1]], CNVdb$OMIMGRCh38)
+##' gdbList[[2]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ClinGen_TriHaploGRCh37, n = 2)[[1]]
+##' gdbList[[3]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$DECIPHER_Haplo, n = 2)[[1]]
+##' gdbList[[4]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ExAC_pLI, n = 2)[[1]]
 ##' gdbList[[5]] <- AnnoCNVGeneRefGene2DDG2P(refGene[[1]], CNVdb$DECIPHER_DDG2P)
 ##'
-##' geneTable <- SunMergeGenesdb(gdbList)
+##' geneTable <- SunMergeGenedb(gdbList)
 ##'
 ##' ## cnsTable <- cns %>% SunCNVTable(hg38cyto, sampleType = 'proband', n = CORENUM)
 ##' ## CrossRegionGeneTable(cnsTable, geneTable)
 ##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
 ##' @importFrom magrittr %<>% %>%
-##' @importFrom stringr str_trim
+##' @importFrom dplyr rename select group_by distinct ungroup mutate
 ##' @rdname mergegene
 ##' @export
 ##'
-SunMergeGenesdb <- function(gdbList) {
+SunMergeGenedb <- function(gdbList) {
 
-  ## process dbs
-  gdb <- gdbList
-  gdb[[1]] %<>% `[`(, c(1:5, 12:14, 6, 7, 10, 11), drop = FALSE)
-  gdb[[2]] %<>% `[`(, c(1:5, 10:20), drop = FALSE)
-  gdb[[3]] %<>% `[`(, c(1:5, 7), drop = FALSE)
-  gdb[[4]] %<>% `[`(, c(1:5, 22), drop = FALSE)
-  gdb[[5]] %<>% `[`(, c(1:5, 8:13), drop = FALSE)
+  ## step1: process dbs
+  gdbList[[1]] %<>%
+    rename(gene_symbol = `Approved Symbol`) %>%
+    select(CNV:overlap_relation, gene_symbol, Phenotypes, PhenotypesCN, MIMNumber, MIMType) %>%
+    group_by(CNV) %>%
+    distinct(gene_symbol, .keep_all = TRUE) %>%
+    ungroup
 
-  res <- gdb[[1]] %>% cbind.data.frame(.,
-                                       matrix('',
-                                              nrow = nrow(gdbList[[1]]),
-                                              ncol = 19),
-                                       stringsAsFactors = FALSE)
-  cnames <- c(colnames(gdb[[1]]),
-              colnames(gdb[[2]])[-1:-5],
-              colnames(gdb[[3]])[-1:-5],
-              colnames(gdb[[4]])[-1:-5],
-              colnames(gdb[[5]])[-1:-5])
 
-  colnames(res) <- cnames
+  gdbList[[2]] %<>%
+    rename(gene_symbol = `Gene Symbol`) %>%
+    select(CNV:overlap_relation, gene_symbol, `Haploinsufficiency Score`, `Triplosensitivity Score`) %>%
+    group_by(CNV) %>%
+    distinct(gene_symbol, .keep_all = TRUE) %>%
+    ungroup
 
-  ## OMIMkey
-  k <- gdbList[[1]][, 'Gene.Symbols']
+  gdbList[[3]] %<>%
+    mutate(gene_symbol = name %>% strsplit(split = '|', fixed = TRUE) %>% sapply(`[`, 1) %>% unlist) %>%
+    select(CNV:overlap_relation, gene_symbol, haploinsufficiency) %>%
+    group_by(CNV) %>%
+    distinct(gene_symbol, .keep_all = TRUE) %>%
+    ungroup
 
-  ## search ClinGenTriHaplo
-  m <- gdb[[2]]
-  tk <- gdbList[[2]][, 'Gene.Symbol']
-  for(i in seq_along(tk)) {
-    midx <- MatchString(tk[i], k)
-    if (is.na(midx)) {
-      ## add loc
-      addEmpty <- c(m[i, 1:5],
-                    tk[i],
-                    rep('', 6),
-                    m[i, -1:-5],
-                    rep('', 8))
+  gdbList[[4]] %<>%
+    rename(gene_symbol = gene) %>%
+    select(CNV:overlap_relation, gene_symbol, pLI) %>%
+    group_by(CNV) %>%
+    distinct(gene_symbol, .keep_all = TRUE) %>%
+    ungroup
 
-      names(addEmpty) <- cnames
+  gdbList[[5]] %<>%
+    rename(gene_symbol = gene.symbol) %>%
+    select(CNV:overlap_relation, gene_symbol, gene.mim:phenotypes) %>%
+    group_by(CNV) %>%
+    distinct(gene_symbol, .keep_all = TRUE) %>%
+    ungroup
 
-      res %<>% rbind.data.frame(addEmpty, stringsAsFactors = FALSE)
-    } else {
-      res[midx, 13:23] <- m[i, -1:-5]
-    }
-  }
+  ## res %<>% GenedbPost
+  gdb <- Reduce(mergeTwoGenedb_, gdbList)
 
-  ## search DECIPHERHaplo
-  k <- res[, 'Gene.Symbols']
-  m <- gdb[[3]]
-  tk <- gdbList[[3]][, 'name'] %>%
-    strsplit(split = '|', fixed = TRUE) %>%
-    sapply(`[`, 1)
-  for(i in seq_along(tk)) {
-    midx <- MatchString(tk[i], k)
-    if (is.na(midx)) {
-      ## add loc
-      addEmpty <- c(m[i, 1:5],
-                    tk[i],
-                    rep('', 17),
-                    m[i, -1:-5],
-                    rep('', 7))
-
-      names(addEmpty) <- cnames
-
-      res %<>% rbind.data.frame(addEmpty, stringsAsFactors = FALSE)
-    } else {
-      res[midx, 24] <- m[i, -1:-5]
-    }
-  }
-
-  ## search ExACpLI
-  k <- res[, 'Gene.Symbols']
-  m <- gdb[[4]]
-  tk <- gdbList[[4]][, 'gene']
-  for(i in seq_along(tk)) {
-    midx <- MatchString(tk[i], k)
-    if (is.na(midx)) {
-      ## add loc
-      addEmpty <- c(m[i, 1:5],
-                    tk[i],
-                    rep('', 18),
-                    m[i, -1:-5],
-                    rep('', 6))
-
-      names(addEmpty) <- cnames
-
-      res %<>% rbind.data.frame(addEmpty, stringsAsFactors = FALSE)
-    } else {
-      res[midx, 25] <- m[i, -1:-5]
-    }
-  }
-
-  ## search DDG2P
-  k <- res[, 'Gene.Symbols']
-  m <- gdb[[5]]
-  tk <- gdbList[[5]][, 'gene.symbol']
-  for(i in seq_along(tk)) {
-    midx <- MatchString(tk[i], k)
-    if (is.na(midx)) {
-      ## add loc
-      addEmpty <- c(m[i, 1:5],
-                    tk[i],
-                    rep('', 19),
-                    m[i, -1:-5])
-
-      names(addEmpty) <- cnames
-
-      res %<>% rbind.data.frame(addEmpty, stringsAsFactors = FALSE)
-    } else {
-      res[midx, 26:31] <- m[i, -1:-5]
-    }
-  }
-
-  res %<>% GenedbPost
-
-  return(res)
+  return(gdb)
 }
 
 
-## ##' @param inputStr A \code{string}.
-## ##' @param searchStr A \code{string vector}.
-## ##' @return A processed \code{data.frame}.
-## ##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
-## ##' @rdname mergegene
-## ##' @importFrom magrittr %>%
-## ##' @importFrom stringr str_trim
-## ##' @keywords internal
-## ##'
-## MatchString <- function(inputStr, searchStr) {
-##   sRes <- sapply(searchStr, function(x) {
-##     sStr <- x %>%
-##       strsplit(split = ',', fixed = TRUE) %>%
-##       unlist %>%
-##       str_trim
+##' Internal Sun's template annotation functions.
+##'
+##' \itemize{
+##'   \item \code{mergeTwoGenedb_()}: Merge two gene results.
+##' }
+##' @title Internal Sun annotation functions
+##' @param gdb1, gdb2: Annotated gene databases.
+##' @return \itemize{
+##'   \item \code{mergeTwoGenedb_()}: A \code{tbl_df}.
+##' }
+##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
+##' @importFrom magrittr %>% %<>%
+##' @importFrom dplyr rename mutate if_else select full_join
+##' @keywords internal
+##'
+mergeTwoGenedb_ <- function(gdb1, gdb2) {
 
-##     return(inputStr %in% sStr)
-##   })
+  ## join
+  gdb <- full_join(gdb1, gdb2, by = c('CNV', 'gene_symbol'))
 
-##   return(which(sRes)[1])
-##   ## return(which(str_detect(searchStr, inputStr))[1])
-## }
+  ## remove NA
+  gdb %<>%
+    rename(chromosome = chromosome.x, start = start.x, end = end.x, overlap_relation = overlap_relation.x) %>%
+    mutate(chromosome = chromosome %>% {if_else(is.na(.), chromosome.y, .)}) %>%
+    mutate(start = start %>% {if_else(is.na(.), start.y, .)}) %>%
+    mutate(end = end %>% {if_else(is.na(.), end.y, .)}) %>%
+    mutate(overlap_relation = overlap_relation %>% {if_else(is.na(.), overlap_relation.y, .)}) %>%
+    select(-(chromosome.y:overlap_relation.y))
+
+  return(gdb)
+}
 
 
 ## ##' @param mergeTable A \code{data.frame} of processed gene table.
@@ -395,7 +322,7 @@ AnnoCNVGeneRefGene2OMIM <- function(refgeneTable, OMIMdb) {
   ## step2: combine with OMIM db and replce OMIM db with GRCh37 location info
   res <- OMIMdb %>%
     select(-(chromosome:end)) %>%
-    inner_join(refTable, ., c('Entrez' = 'Entrez'))
+    inner_join(refTable, ., by = 'Entrez')
 
   return(res)
 }
@@ -432,10 +359,21 @@ AnnoCNVGeneRefGene2DDG2P <- function(refgeneTable, DDG2Pdb) {
 
   ## step2: combine with OMIM db and replce OMIM db with GRCh37 location info
   res <- DDG2Pdb %>%
-        inner_join(refTable, ., c('Entrez' = 'Entrez'))
+        inner_join(refTable, ., by = 'Entrez')
 
   return(res)
 }
 
 
 
+
+
+## refGene <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$RefGeneGRCh37, n = 2)
+## gdbList <- list()
+## gdbList[[1]] <- AnnoCNVGeneRefGene2OMIM(refGene[[1]], CNVdb$OMIMGRCh38)
+## gdbList[[2]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ClinGen_TriHaploGRCh37, n = 2)[[1]]
+## gdbList[[3]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$DECIPHER_Haplo, n = 2)[[1]]
+## gdbList[[4]] <- AnnoCNVBatch(kit, AnnoCNVGeneCore, CNVdb$ExAC_pLI, n = 2)[[1]]
+## gdbList[[5]] <- AnnoCNVGeneRefGene2DDG2P(refGene[[1]], CNVdb$DECIPHER_DDG2P)
+
+## geneTable <- SunMergeGenesdb(gdbList)
