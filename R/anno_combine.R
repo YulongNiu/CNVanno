8556afb8feb298e82169ff93015f6f5bcfdaff97
Yulong Niu <yulong.niu@hotmail.com>
2018-12-15 23:09:13 +0800

    add compress params of CNVRaw

---
+++ R/anno_combine.R
@@ -0,0 +1,71 @@
+x##' @include AllClasses.R AllGenerics.R
+NULL
+
+
+##' Combine parameters from the raw CNV outputs
+##'
+##' Merge the parameters from the raw CNV outpus.
+##' @title Combine parameters from \code{RawCNV}
+##' @inheritParams CombinePara
+##' @return A \code{tbl_df} object.
+##' @examples
+##' require('magrittr')
+##' require('dplyr')
+##' data(nator)
+##' data(kit)
+##'
+##' mergecnv <- Merge(list(nator, kit), reciprate = 0.5, n = 2)
+##' kitraw <- system.file('extdata', 'example.cnvkit', package = 'CNVanno') %>% read_cnvkit
+##' kitraw@params %<>% select(log2, cn)
+##'
+##' tmp1 <- 
+##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
+##' @importFrom magrittr %>%
+##' @importFrom dplyr do group_by ungroup select everything
+##' @importFrom tibble tibble
+##' @importFrom methods new
+##' @rdname CombinePara-methods
+##' @exportMethod CombinePara
+##'
+SetMethod(f = 'CombinePara',
+          signature = c(core = 'CoreCNV', raw = 'RawCNV', reciprate = 'numeric'),
+          definition = function(core, raw, reciprate, ...) {
+
+          })
+
+
+##' CombinePara internal functions
+##'
+##' \itemize{
+##'   \item \code{compressPara_()}: Compress the parameters of \code{RawCNV} into one column and combine it with the `rawCNV` slot.
+##'   \item \code{combineRow_()}: Combine each row of the \code{RawCNV}.
+##' }
+##' @title Internal functions for CombinePara
+##' @param raw A \code{RawCNV}.
+##' @return
+##' \itemize{
+##'   \item \code{compressPara_()}: A \code{tbl_df}.
+##'   \item \code{combineRow_()}: A \code{string}.
+##' }
+##' @author Yulong Niu \email{yulong.niu@@hotmail.com}
+##' @importFrom magrittr %>%
+##' @importFrom dplyr bind_cols expr funs mutate_if transmute_all
+##' @importFrom tidyr unite
+##' @rdname combineparautility
+##' @keywords internal
+##'
+compressPara_ <- function(raw) {
+
+  res <- raw@params %>%
+    mutate_if(is.numeric, round, 2) %>%
+    transmute_all(.funs = funs(paste(expr(.), ., sep = '='))) %>%
+    unite(params, 1 : ncol(.), sep = ',') %>%
+    bind_cols(raw@rawCNV, .)
+
+  return(res)
+}
+
+
+combineRow_ <- function(corerow, rawpara, reciprate) {
+
+}
